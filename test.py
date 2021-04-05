479683bf829a15a8ad2496f32bd37cd19270d28d
coderMachineLee <lichen.oregon@gmail.com>
2021-04-04 21:39:12 -0700

    intit

---
+++ test.py
@@ -0,0 +1,24 @@
+import math
+import os
+import sys
+
+import requests
+
+r = requests.get('https://google.com')
+print(r.status_code)
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who):
+    test = "asdf"
+    greeting = f"hello, {who}"
+    return greeting
+
+
+print(greet('world'))
+print(greet('lee'))
+
+name = input("?")
+print(name)
